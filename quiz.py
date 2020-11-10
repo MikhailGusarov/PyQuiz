440d571f475c9f6f542d8ea7e429fb1c256897d6
GusarovMV <vgg639@gmail.com>
2020-11-10 17:17:27 +0300

    добавил категории

--- quiz.py
+++ quiz.py
@@ -1,8 +1,5 @@
 class Team:
     """Команда-участник"""
-    name = ''
-    count_points = 0
-
     def __init__(self, name):
         self.name = name
 
@@ -12,24 +9,31 @@ class Team:
 
 class Question:
     """Вопрос"""
-    content_question = ''
-    correct_answer = ''
-    count_points = 0
-
     def __init__(self, content_question, correct_answer, count_point):
         self.content_question = content_question
         self.correct_answer = correct_answer
         self.count_points = count_point
 
     def __str__(self):
-        return self.content_question
+        return self.count_point
 
 
-questions = [Question('Сколько будет 2+2', '4', 1),
-             Question('Если рыба-меч с острым носом, то птица с острым клювом - это?', 'жаба', 5),
-             Question('Я ему сын сына, а он мне', 'дед', 3),
-             Question('Острая кувалда', 'секира', 2),
-             ]
+class Category:
+    """Категория вопросов"""
+    def __init__(self, name, questions):
+        self.name = name
+        self.questions = questions
+
+    def __str__(self):
+        return self.name
+
+
+questions_category = [Question('Сколько будет 2+2', '4', 1),
+                      Question('Если рыба-меч с острым носом, то птица с острым клювом - это?', 'жаба', 5),
+                      Question('Я ему сын сына, а он мне', 'дед', 3),
+                      Question('Острая кувалда', 'секира', 2)]
+
+test_category = Category('Просто котегория', questions_category)
 
 teams = []
 
@@ -41,5 +45,3 @@ for i in range(int(count_teams)):
     teams.append(Team(team_name))
 
 
-for questions in questions:
-    print(questions)
