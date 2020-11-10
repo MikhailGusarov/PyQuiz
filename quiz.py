632fdebb546a37022f07f810eb11916160d3cb26
GusarovMV <vgg639@gmail.com>
2020-11-10 16:59:44 +0300

    реорганизация кода

--- quiz.py
+++ quiz.py
@@ -1,5 +1,28 @@
-from team import Team
-from question import Question
+class Team:
+    """Команда-участник"""
+    name = ''
+    count_points = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Question:
+    """Вопрос"""
+    content_question = ''
+    correct_answer = ''
+    count_points = 0
+
+    def __init__(self, content_question, correct_answer, count_point):
+        self.content_question = content_question
+        self.correct_answer = correct_answer
+        self.count_points = count_point
+
+    def __str__(self):
+        return self.content_question
 
 
 questions = [Question('Сколько будет 2+2', '4', 1),
@@ -18,3 +41,5 @@ for i in range(int(count_teams)):
     teams.append(Team(team_name))
 
 
+for questions in questions:
+    print(questions)
