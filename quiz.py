fb6c0da215ef5acb1a3e47e2e91cd91692b7c9ae
GusarovMV <vgg639@gmail.com>
2020-11-10 17:36:25 +0300

    i поненял на _

--- quiz.py
+++ quiz.py
@@ -40,8 +40,7 @@ teams = []
 print('Добро пожаловать в игру "Викторина"')
 count_teams = input('Введите количество команд: ')
 
-for i in range(int(count_teams)):
+for _ in range(int(count_teams)):
     team_name = input('Введите название команды: ')
     teams.append(Team(team_name))
-
 
