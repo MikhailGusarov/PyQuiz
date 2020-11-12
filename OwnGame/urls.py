bc1db46671cfc5e7eda6db09bb42fb1e9193d0c6
GusarovMV <vgg639@gmail.com>
2020-11-12 17:49:41 +0300

    Не работают вопросы

--- OwnGame/urls.py
+++ OwnGame/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'owngame'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:game_number>/', views.game, name='game')
+    path('<int:game_number>/', views.game, name='game'),
+    path('<int:game_number>/<int:id_category>/<int:count_points>', views.question)
 ]
--- OwnGame/views.py
+++ OwnGame/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Game
+from .models import Game, Question
 
 
 def index(request):
@@ -13,3 +13,9 @@ def game(request, game_number):
     current_game = get_object_or_404(Game, number=game_number)
     context = {'game': current_game}
     return render(request, 'game.html', context)
+
+
+def question(request, game_number, id_category, count_points):
+    current_question = get_object_or_404(Question, count_points=count_points, category=id_category)
+    context = {'question': current_question}
+    return render(request, 'question.html', context)
