e26a5cbb779c3c9d66b09ba49c6360b6c44cd64d
MikhailGusarov <vgg639@gmail.com>
2020-11-12 22:57:02 +0300

    ааааа

--- OwnGame/urls.py
+++ OwnGame/urls.py
@@ -6,5 +6,5 @@ app_name = 'owngame'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:game_number>/', views.game, name='game'),
-    path('<int:game_number>/<int:id_category>/<int:count_points>', views.question)
+    path('<int:game_number>/<int:category_id>/<int:count_points>', views.question, name='question')
 ]
