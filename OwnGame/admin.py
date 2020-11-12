275d905ae621c6cab7da1c247b65a9489d5ba265
GusarovMV <vgg639@gmail.com>
2020-11-12 17:17:08 +0300

    Удалил всё, что мог

--- OwnGame/admin.py
+++ OwnGame/admin.py
@@ -4,8 +4,3 @@ from .models import *
 admin.site.register(Game)
 admin.site.register(Category)
 admin.site.register(Question)
-admin.site.register(Team)
-admin.site.register(Leading)
-admin.site.register(Match)
-admin.site.register(MatchTeam)
-admin.site.register(MatchQuestion)
--- OwnGame/models.py
+++ OwnGame/models.py
@@ -38,7 +38,7 @@ class Category(models.Model):
 
 
 class Question(models.Model):
-    """Обычный вопрос"""
+    """Вопрос"""
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
                                  verbose_name='Категория', help_text='Выберите категорию')
     content_question = models.TextField(verbose_name='Вопрос', help_text='Введите вопрос')
@@ -47,52 +47,3 @@ class Question(models.Model):
 
     def __str__(self):
         return f'{self.category} Цена: {str(self.count_points)}'
-
-
-class Team(models.Model):
-    """Команда-участник"""
-    name = models.CharField(verbose_name='Название команды', max_length=200, help_text='Введите название команды')
-    count_points = models.PositiveIntegerField(verbose_name='Количество баллов команды', default=0)
-
-    def __str__(self):
-        return self.name
-
-
-class Leading(models.Model):
-    """Ведущий"""
-    name = models.CharField(verbose_name='Имя', max_length=200)
-
-    def __str__(self):
-        return self.name
-
-
-class Match(models.Model):
-    """Конкретный матч"""
-    number = models.PositiveIntegerField(verbose_name='Номер', unique=True)
-    game = models.ForeignKey(Game, on_delete=models.CASCADE, verbose_name='Игра')
-    leading = models.ForeignKey(Leading, on_delete=models.CASCADE, verbose_name='Ведущий')
-    date_start = models.DateTimeField(verbose_name='Время начала матча', null=True, blank=True)
-    date_end = models.DateTimeField(verbose_name='Время окончания матча', null=True, blank=True)
-
-    def __str__(self):
-        return f'Матч №{str(self.number)} {self.game}'
-
-
-class MatchTeam(models.Model):
-    """Команды в матче"""
-    play = models.ForeignKey(Match, on_delete=models.CASCADE, verbose_name='Матч')
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name='Команда')
-    count_points = models.PositiveIntegerField(verbose_name='Очки за матч', default=0)
-
-    def __str__(self):
-        return f'Команда: {str(self.team)} Матч №{self.play.number}'
-
-
-class MatchQuestion(models.Model):
-    """Вопросы в матче"""
-    play = models.ForeignKey(Match, on_delete=models.CASCADE, verbose_name='Матч')
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Вопрос')
-    is_done = models.BooleanField(verbose_name='Отвечен', default=False)
-
-    def __str__(self):
-        return f'{self.question} Матч №{self.play.number}'
