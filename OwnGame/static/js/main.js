905cfd19c023da5e2be117a188fdf6898b80db8c
GusarovMV <vgg639@gmail.com>
2020-11-19 12:23:19 +0300

    проправил js

--- OwnGame/static/js/main.js
+++ OwnGame/static/js/main.js
@@ -1,5 +1,7 @@
-document.getElementsByClassName('show_answer')[0].onclick = function(){
-    document.getElementsByClassName('show_answer')[0].style = 'display:none';
-    document.getElementsByClassName('back')[0].style = 'display:block';
-    document.getElementsByClassName('answer')[0].style = 'display:block';
+if(document.getElementsByClassName('show_answer')[0] != undefined) {
+    document.getElementsByClassName('show_answer')[0].onclick = function(){
+        document.getElementsByClassName('show_answer')[0].style = 'display:none';
+        document.getElementsByClassName('back')[0].style = 'display:block';
+        document.getElementsByClassName('answer')[0].style = 'display:block';
+    }
 }
