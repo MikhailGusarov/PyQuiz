e118c5dc7644bbf49f0f5047f3e34a124cd87860
GusarovMV <vgg639@gmail.com>
2020-11-19 11:22:33 +0300

    CSS и JS страницы вопроса

---
+++ OwnGame/static/js/main.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName('show_answer')[0].onclick = function(){
+    document.getElementsByClassName('show_answer')[0].style = 'display:none';
+    document.getElementsByClassName('back')[0].style = 'display:block';
+    document.getElementsByClassName('answer')[0].style = 'display:block';
+}
